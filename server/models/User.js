7bfa3ea214900f9e1e0bbffcdb6a50ca3e7a2923
Jesse <jesse_pasanen@hotmail.com>
2021-12-14 21:30:08 +0200

    backend for register and login

---
+++ server/models/User.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+let userSchema = new Schema ({
+    username: {type: String},
+    email: {type: String},
+    password: {type: String},
+    registerdate: {type: Date, default: Date.now},
+    admin: {type: Boolean}
+});
+
+module.exports = mongoose.model("users", userSchema);
